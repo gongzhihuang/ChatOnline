8bd9793c90f769d50a7996d5cdf6f8e9246ac5e3
forestGzh <734924401@qq.com>
2020-08-28 15:23:07 +0800

    add TestChat.vue

--- chat-online.client/src/views/Home.vue
+++ chat-online.client/src/views/Home.vue
@@ -2,18 +2,21 @@
   <div class="home">
     <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
     <Chat />
+    <!-- <TestChat /> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from "@/components/HelloWorld.vue";
+// import TestChat from "@/components/TestChat.vue";
 import Chat from "@/components/Chat.vue";
 
 export default {
   name: "Home",
   components: {
     // HelloWorld
+    // TestChat
     Chat
   }
 };
